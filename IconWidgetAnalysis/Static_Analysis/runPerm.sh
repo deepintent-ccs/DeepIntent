f2b93a19c866042a5de4a1ad77258c9805b41fc9
Shao Yang <yangshao05311@gmail.com>
2019-11-23 23:39:15 -0500

    Delete runPerm.sh

--- IconWidgetAnalysis/Static_Analysis/runPerm.sh
+++
@@ -1,7 +0,0 @@
-
-
-for app in `ls /Users/shaoyang/Desktop/DeepIntent_Example/benign/*.apk`
-do
-echo $app
-java -jar /Users/shaoyang/Downloads/Static_Analysis/APKCallGraph.jar $app /Users/shaoyang/Desktop/DeepIntent_Example/benign/ /Users/shaoyang/Downloads/Static_Analysis/img2widgets/ /Users/shaoyang/Downloads/Static_Analysis/permission_output/ /Users/shaoyang/Downloads/Static_Analysis/ic3/output/
-done
