828c70d0b79976964e77b342f9e4d0ad6cc35019
cellzero <cellzero@sina.cn>
2020-03-03 21:11:49 +0800

    Update README of itention dehavior discrepancy, and remove some unused codes.

---
+++ IntentionBehaviorDiscrepancy/README.md
@@ -0,0 +1,31 @@
+# Intention Behavior Discrepancy
+
+Intention behavior discrepancy aims to detect outliers according to co-attentioned feature and prediction result of DeepIntent model. It uses the co-attentioned feature to train the AutoEncoder outlier detection model, then combines distance-based aggregation and prediction-based aggregation to compute final outlier score.
+
+## Introduction
+
+This folder mainly contains the codes to train and evaluate the outlier model. It also includes codes to load the pre-trained deep learning model, load training and labeled testing data, and so on. Next, we briefly introduce each Python file.
+
++ conf.py: configuration of the pre-trained deep learning model, used to ensure the input data shape.
++ layers.py: co-attention structure, to load the deep learning model.
++ metrics.py: to compute the precision, recall, AUC value of the labeled testing data.
++ outlier.py: training and testing the outlier detection model.
+
+## Requirements
+
++ Python >= 3.6.0
++ numpy >= 1.16.0
++ Pillow >= 5.4.1 (PIL)
++ Keras >= 2.2.4
++ nltk >= 3.4.0
++ pyod >= 0.7.4
++ sklearn >= 0.21.1
++ matplotlib >= 3.0.2, optional, to plot precision and recall curve
+
+## Entry Point
+
+There are mainly 1 executable Python scripts as entry point:
+
+### Outlier
+
+Load the pre-trained deep learning model to extract co-attentioned features and prediction results. Then, train and test the outlier detection model. Directly run `python3 outlier.py` will handle the data stored in `data/total`, which could be download from the BaiduYun.
--- IntentionBehaviorDiscrepancy/conf.py
+++ IntentionBehaviorDiscrepancy/conf.py
@@ -3,19 +3,6 @@
 from collections import namedtuple
 
 
-PreProcessConf = namedtuple('PreProcessConf', [
-    # path
-    'path_data_in',         # String, path of the raw data (after contextual text extraction)
-    'path_data_out',        # String, path to save the pre-process data
-    # image
-    'image_min_size',       # Int, image's width and height should be higher than this value
-    'image_wh_ratio',       # Int or float, the ratio between width and height should not be higher than this value
-    # text
-    'text_min_support',     # Int, text appeared less than the threshold will be removed
-    # permissions
-    'target_groups',        # Dict, map permissions to groups, the key of the dict is group name
-])
-
 ModelConf = namedtuple('ModelConf', [
     # image feature
     'img_shape',            # Tuple of int, (width, height, channels) of the image
@@ -38,59 +25,3 @@ ModelConf = namedtuple('ModelConf', [
     # attention
     'att_dim',              # Int, inner attention dim K
 ])
-
-TrainConf = namedtuple('TrainConf', [
-    # train (and test) the model
-    'code_name',            # String, the model name (for human understanding)
-    'repeat_times',         # Int, how many times to train the model (for cross validation)
-    'random_seed',          # Int, the random seed, to fix the data order
-    # path
-    'path_data',            # String, path of the training data
-    'path_output',          # String, folder to contain the outputs
-    # data partition
-    'train_ratio',          # Float, training ratio to split data
-    'valid_ratio',          # Float, validation ratio to split data
-    'test_ratio',           # Float, testing ratio to split data
-    'is_data_refresh',      # Boolean, whether to refresh data after each training process
-    # callbacks
-    'early_stop_patients',  # Int, Keras early stop parameter
-    # fit parameters
-    'batch_size',           # Int, the size of each batch
-    'epochs',               # Int, the number of epochs
-    'verbose',              # Int, verbose in Keras, 0 to 2, silent to verbose
-    'monitor_type',         # String, could be 'val_loss' or 'val_acc'
-    # log option
-    'is_log_history',       # Boolean, whether to save the training history
-    'is_log_prediction',    # Boolean, whether to save the predictions
-    'is_log_avg_score',     # Boolean, whether to save the average scores
-])
-
-PredictConf = namedtuple('PredictConf', [
-    # use the pre-trained model to predict the results
-    # path
-    'path_data',            # String, path of data
-    'path_meta',            # String, path of meta data of the pre-trained model
-    'path_model',           # String, path of the pre-trained model
-    'path_predictions',     # String, path to save the predictions
-    # prediction
-    'threshold',            # Float, the value to , commonly could be set to 0.5
-])
-
-
-def check_conf(value, domain, fallback):
-    """Keep the conf value within the specific domain.
-    """
-    return value if value in domain else fallback
-
-
-target_groups = {
-    'NETWORK': {'INTERNET', 'CHANGE_WIFI_STATE'},
-    'LOCATION': {'ACCESS_COARSE_LOCATION', 'ACCESS_FINE_LOCATION', 'ACCESS_MOCK_LOCATION'},
-    'MICROPHONE': {'RECORD_AUDIO'},
-    'SMS': {'SEND_SMS', 'READ_SMS', 'WRITE_SMS', 'RECEIVE_SMS'},
-    'CAMERA': {'CAMERA'},
-    'CALL': {'CALL_PHONE'},
-    'STORAGE': {'WRITE_EXTERNAL_STORAGE', 'READ_EXTERNAL_STORAGE'},
-    'CONTACTS': {'READ_CONTACTS', 'WRITE_CONTACTS',
-                 'GET_ACCOUNTS', 'MANAGE_ACCOUNTS', 'AUTHENTICATE_ACCOUNTS'},
-}
